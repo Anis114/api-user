39847d6d60cb10087d2ed15484190743d757d256
Anis114 <93031600+Anis114@users.noreply.github.com>
2022-08-16 15:18:15 +0100

    hi

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -5,7 +5,13 @@ const UserList =({users})=> {
         return (<ul className='"list-group'>
       {users.map((user)=>(
         <li className="list-group-itme d-flex justify-content-between align-items-center">
+            <h1>{user.username}</h1>
+            {user.id}
             {user.name}
+            {user.email}
+            {user.address}
+            {user.street}
+            {user.suite}
         </li>
     
      ))};
